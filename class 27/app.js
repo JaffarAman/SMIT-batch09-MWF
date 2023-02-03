d1ad9836497f180acfa6edda7b1655231e94421b
jaffar aman <amanjaffar50@gmail.com>
2023-02-03 10:59:18 +0500

    JS Variable

---
+++ class 27/app.js
@@ -0,0 +1,83 @@
+// alert("HELLO WORLD");
+
+
+
+// Variables
+
+// var myname = "Hello world";
+// var myname = "hello class";  // variable
+
+
+// alert(myname);
+
+
+
+// var std1 = "Jaffar Aman";
+// var std2 = "Sufiyan";
+// var std3 = "Bilal";
+
+
+
+
+
+
+// // String Variable
+// var userName  = "Talha"  ///String Type
+// // var userEmail = ""
+
+// // Number Variable//  20 21 50 100 1 0 1000
+
+// var userAge = 20
+
+
+// alert(  userAge  )
+
+
+
+
+
+
+// var  variableName = variableValue;
+
+
+// var myName = "Jaffar";
+// // alert(myName)
+
+
+// var userName  = myName;
+
+
+// DATA TYPES   ==> data =  values
+
+// String   /// group OR Set of char   "  "  OR '   ' eg, "jaffar" , "jaff@"
+// Number  //Number eg: 20 10 500
+// Boolean //true false
+// undefined
+// null
+
+
+var stringVar = "jaffar10@gmail.com"
+
+var numberVar = 20;   //Number Data type
+// alert(stringVar)
+
+var isActive = true  
+
+alert(isActive)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
