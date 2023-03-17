716eb25ae8a5b78f4b87760bb6b3dc33d6adab38
jaffar aman <amanjaffar50@gmail.com>
2023-03-17 10:32:17 +0500

    function start

---
+++ class 44/app.js
@@ -0,0 +1,91 @@
+// console.log("HELLO CLASS..")
+
+// functions
+
+
+// console.log("HELLO WORLD");
+// console.log("HELLO CLASS");
+
+// function greetUser() {
+//     alert("HELLO USER")
+// }
+// greetUser()
+
+// alert("JAFFAR AMAN")
+
+// function sum() {
+//     // alert("HELLO USER")
+//     var num1 = +prompt("Enter Number 1")
+//     var num2 = +prompt("Enter Number 2")
+//     console.log("sum of two numbers = " + (num1 + num2))
+
+// }
+
+
+// sum()
+
+// BLOCK OF CODES
+
+// sum()
+
+
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+
+
+// function greetUser(userName) {
+//     console.log(userName, "userName")
+//     console.log("HELLO " + userName)
+// }
+
+// greetUser("ALI")
+// greetUser("Ayan")
+
+// parameter  VALUE RECEIVED
+// arguments  VALUE PASS
+
+// function addition(num1, num2) {
+//     console.log(num1, "num1")
+//     console.log(num2, "num2")
+//     console.log(num1 + num2, "addition")
+// }
+
+// // userNum1 = prompt
+// // userNum2 = prompt
+// addition(20, 50)
+
+
+// function logger(parameter) {
+//     console.log(parameter)
+// }
+
+
+// function sum(num1, num2 = 0) {
+//     console.log(num2, "num2")
+//     console.log(num1 + num2)
+// }
+
+
+// sum(500)
+
+// logger("HELLO WORLD")
+
+
+
+
+function cal(num1, num2, opt) {
+
+    if (opt === "+") {
+        console.log(num1 + num2, "addition")
+    }
+
+
+}
+
+var num1 = +prompt()
+var num2 = +prompt()
+var opt = prompt(opt)
+cal(num1, num2, opt)
+// cal(30, 50, "+")
