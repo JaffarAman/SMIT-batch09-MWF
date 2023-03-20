2d38bcd64b93949e9272ad69934d0f815168aa56
jaffar aman <amanjaffar50@gmail.com>
2023-03-20 10:49:15 +0500

    function end

---
+++ class 45/app.js
@@ -0,0 +1,78 @@
+// console.log("HELLO CLASS")
+
+
+
+
+
+// function greetUser(userName, age) {
+//     console.log("HELLO " + userName, age)
+//     // alert("thanks for your feedback..")
+// }
+
+// greetUser()
+
+// function sum() {
+//     var num1 = +prompt("enter num1")
+//     var num2 = +prompt("enter num2")
+//     console.log(num1 + num2)
+
+// }
+
+
+
+// function foo() {
+//     ///
+//     var arr = ["hello", "world"]
+//     return arr
+// }
+
+// console.log(foo(), "foo()")
+
+// var number1 = 100
+// var number2 = 50
+
+
+// function sum(num1 = 0, num2 = 0) {
+//     return num1 + num2;
+
+//     console.log("hello")
+
+
+
+// }
+
+
+
+// var result = sum(number1, number2)
+// console.log(result, "result")
+
+
+// var userName = "ALi"   /// GLOBAL SCOPE
+
+
+// function foo() {
+//     // var userName = "Jaffar Aman"  //local scope
+//     var userName = "JAFFAR AMAN"
+//     console.log(userName)
+
+// }
+
+
+
+// foo()
+
+// console.log(userName)
+
+
+
+
+
+var userName = "Jaffar"
+function foo() {
+     userName = "Jaffar aman"
+}
+
+
+foo()
+
+console.log(userName)
