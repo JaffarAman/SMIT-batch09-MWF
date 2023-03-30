affaa974467ba9a65ec5d2c7f295f8cd887f521b
innosufiyan <innosufiyan@gmail.com>
2023-03-30 07:57:03 +0500

    JS revision

---
+++ class 48/app.js
@@ -0,0 +1,40 @@
+
+// var input = document.getElementById("textField")
+
+// function setNumber(num) {
+
+
+//     // console.log(input.value, "input")
+//     // input.value = input.value + n
+//     input.value += num
+
+
+
+// }
+
+
+// function results() {
+//     var result = eval(input.value)
+//     console.log(result)
+//     input.value = result
+
+// }
+
+
+// function allClear() {
+//     input.value = ""
+// }
+// function clearNum() {
+//     var inputValue = input.value
+//     // console.log(input.value.slice(0, -1))
+//     input.value = inputValue.slice(0, -1)
+// }
+
+
+
+
+
+
+function foo() {
+    console.log("function call")
+}
---
+++ class 49/app.js
@@ -0,0 +1,97 @@
+// console.log("HELLO CLASS")
+
+// variables
+
+
+// var firstName = "Jaffar" //String
+// var lastName = undefined;
+// console.log(lastName, "lastName")
+
+
+// Data types
+// String , Number, Boolean ,undefined ,null  //Primitive Data types
+// Array ,function , Object ///non-primitive data types
+
+
+// var userName = "muhammad Ali"
+// userName = "Jaffar"
+// console.log(userName)
+
+// var num = 50
+// num = num + 100
+// // num += "100"
+
+// console.log(num)
+
+
+// var num = 10
+
+// console.log(num++) //10
+// console.log(++num) //11
+// console.log(num) //11
+
+
+
+
+// conditions
+// if else
+
+
+// var city = "lahore"
+
+// if (city === "karachi") {
+//     console.log("Welcome to karachi")
+// } else {
+//     console.log("Welcome to nikal")
+// }
+
+
+
+// var age = 20
+// if (age == "20") {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+
+// var age = 99;
+// if (age >= 20 && age <= 50) {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+// var bool = ""
+// if (bool) {
+//     console.log("true condition")
+// } else {
+//     console.log("false condition")
+
+// }
+
+
+// var bool = true
+// if (bool) {
+
+// }
+
+
+
+
+// var age = 25
+// var gender = "female"
+// // true  || gender === "male"
+// if (age >= 20 && age <= 50 || gender === "male") {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+
+// }
+
+
+// Arrays
+
+
