58d86aad5dc677fe14062f00a0561c14cd192d99
innosufiyan <innosufiyan@gmail.com>
2023-04-01 07:45:29 +0500

    arrays and loop revision.

---
+++ class 50/app.js
@@ -0,0 +1,80 @@
+// Arrays
+// var arr = []
+// console.log(arr, "arr")
+
+
+// var arr = new Array(5)
+// var arr = []
+
+// arr[0] = "Apple"
+// arr[5] = "orange"
+// console.log(arr[2])
+
+
+// var muiltArray = [[1, 2, 3], [4, 5, 6]]
+
+// console.log(muiltArray[0][1])
+
+
+// var arr = ["apple"]
+
+// arr.push("orange", "mango")
+
+// arr.pop()
+
+// arr.unshift("graphs")
+
+// console.log(arr)
+
+
+// var array = ["apple", "mango", "orange"];
+// console.log(typeof array)
+
+// array.pop()
+
+// var arrCopy = array.slice(0, 3)
+
+
+// console.log(array)
+// console.log(arrCopy)
+
+
+
+
+// LOOPS
+// document.write("1 <br />")
+
+
+
+// for (var i = 0; i <= 10; i++) {
+//     document.write(i + "<br />")
+
+// }
+
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == "mango") {
+//         // break
+//         continue
+//     }
+//     console.log(arr[i])
+
+// }.
+
+
+// var arr = ["apple", "mango", "orange",]
+// var flag = false
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == "orange") {
+//         console.log("match")
+//         flag = true
+//         break
+//     }
+
+// }
+
+
+// if (flag === false) {
+//     console.log("is not match")
+// }
