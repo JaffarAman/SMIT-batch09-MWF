ae4acb0af030fbf9ac59f3bf6dd1d9d0893c8071
jaffar aman <amanjaffar50@gmail.com>
2023-05-15 10:49:15 +0500

    switch statement && while || do shile

---
+++ class 61/app.js
@@ -0,0 +1,99 @@
+var age = 30
+// if (age === 20) {
+//     console.log("allow")
+// } else if (age === 25) {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+
+// switch (age) {
+
+//     case 20:
+//         console.log("allow 20");
+//         break
+//     case 25:
+//         console.log("allow 25")
+//         break
+
+//     default:
+//         console.log("not allow")
+
+// }
+
+
+
+// var userRole = "adminSSSSS"
+
+// if (userRole === "admin") {
+//     console.log("allow on admin portal")
+// } else {
+//     console.log("not allow")
+// }
+
+
+
+// switch (userRole) {
+
+//     case "admin":
+//         console.log("allow on admin portal")
+//         break
+//     default:
+//         console.log("not allow")
+
+// }
+
+
+// var age = 20
+// // if (age >= 18) {
+// //     console.log("allow")
+// // } else {
+// //     console.log("not allow")
+// // }
+
+
+// switch (age) {
+//     case age >= 18:
+//         console.log("allow")
+//         break;
+//     default:
+//         console.log("now allow")
+// }
+
+
+
+
+
+
+// var arr = ["apple", "mango",]
+// var i = 0;
+// while (i < arr.length) {
+
+//     // document.write(i)
+//     document.write(i)
+//     console.log(arr[i])
+
+
+
+//     i++
+// }
+
+
+var i = 5
+do {
+    console.log("i", i)
+    i++
+}
+while (i < 0)
+
+
+
+
+
+
+
+
+
+
