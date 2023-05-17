dc8d2d8afe7ca2d668d62716adc3f12464ba6253
amnashahzad <amnashahzad.428264@gmail.com>
2023-05-17 10:49:37 +0500

    revision chap 64 to 68

---
+++ class 62/app.js
@@ -0,0 +1,61 @@
+// console.log(document.childNodes[1].children[1].children[0])
+
+// var ulElement = document.childNodes[1].children[1].children[0]
+
+// console.log("ulElement", ulElement)
+
+
+// var ulElement = document.getElementById("ulElement")
+
+// console.log("ulElement", ulElement.children.length)
+
+
+
+// var input = document.getElementById("input")
+// console.log("input", input.className = "classInput")
+
+
+// hasAttribute(attrName)  return bool
+// getAttribute(attrName) returm
+// setAttribute(attrName , attrValue)
+
+// console.log("input", input.hasAttribute("class"))
+// console.log("input", input.setAttribute("placeholder", "enter name"))
+
+// console.log("input", input.getAttribute("placeholder"))
+// console.log("input", input.attributes[2])
+// input.removeAttribute("class")
+
+
+
+
+// var input = document.getElementById("input")
+// // input.className += " bgColor"
+// // console.log(input.classList.add("addClass"))
+// input.classList.remove()
+
+
+
+// var parent = document.getElementById("parent")
+
+
+// for (var i = 0; i < 10; i++) {
+//     var h1Element = document.createElement("h1")
+//     var h1Txt = document.createTextNode("HEADING " + (i + 1))
+//     h1Element.appendChild(h1Txt)
+//     parent.appendChild(h1Element)
+
+// }
+
+// function calc(a, b) {
+
+
+//     return a + b / 2
+// }
+
+// console.log(calc(2, 1))
+
+
+
+// var arr = [1, "23", 3, 10, 50]
+// console.log(arr.includes(23))
