a1b94f34259d785f1d49e5e66b053920cace5496
jaffar aman <amanjaffar50@gmail.com>
2023-06-26 10:31:49 +0500

    es6 start

---
+++ class 79/app.js
@@ -0,0 +1,69 @@
+// var
+//  let
+//  const
+
+// var fullName = "JAFFAR AMAN"
+// // fullName = "SUFIYAN"
+
+// var fullName = "BILAL"
+
+// console.log(fullName)
+
+
+// let fullName = "JAFFAR"
+// // fullName = "JAFFAR AMAN"
+// let fullName = "JAFFAR AMAN"
+// console.log(fullName)
+
+// // var fullName;
+// let fullName;
+// fullName = "JAFFAR"
+// console.log(fullName)
+
+
+// const fullName = "JAFFAR"
+// // const fullName = "JAFFAR AMAN"
+// fullName = "JAFFAR AMAN"
+
+// const fullName;
+
+// console.log(fullName)
+
+
+
+
+// function foo() {
+//     // function scope
+//     var a = 100
+// }
+
+// if (false) {
+//     var a = 50
+// } else {
+//     var a = 500
+
+// }
+
+// foo()
+// console.log(a)
+
+// let block scope
+
+// function foo() {
+//     let num1 = 100
+//     return num1
+// }
+
+// var result = foo()
+// console.log(result)
+// if (true) {
+//     let num2 = 500
+
+// }
+// console.log(num2)
+// // console.log(num1)
+
+
+
+console.log(b)
+var b = 100
