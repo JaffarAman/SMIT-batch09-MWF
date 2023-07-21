d7d75c998359220892aa55f72c9b4770f5b6c6f7
jaffar aman <amanjaffar50@gmail.com>
2023-07-21 10:27:54 +0500

    firebase auth complete

---
+++ class 85/login.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDc1UsuIg_x7zr0KNbrJzgBMKlIsye9f9w",
+    authDomain: "food-app-d442b.firebaseapp.com",
+    projectId: "food-app-d442b",
+    storageBucket: "food-app-d442b.appspot.com",
+    messagingSenderId: "1016332638211",
+    appId: "1:1016332638211:web:6c6824f0b4b01643119636"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+// Initialize AUTH
+const auth = getAuth();
+
+
+const loginBtn = document.querySelector("#loginBtn")
+loginBtn.addEventListener("click", login)
+
+async function login(e) {
+    try {
+
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+        console.log(email, password)
+        const userLogin = await signInWithEmailAndPassword(auth, email, password)
+        console.log(userLogin)
+        localStorage.setItem("userUid", userLogin.user.uid)
+        window.location.replace("/dashboard.html")
+
+
+    } catch (error) {
+        console.log("error", error.message)
+        alert(error.message)
+    }
+
+
+}
---
+++ class 85/signup.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDc1UsuIg_x7zr0KNbrJzgBMKlIsye9f9w",
+    authDomain: "food-app-d442b.firebaseapp.com",
+    projectId: "food-app-d442b",
+    storageBucket: "food-app-d442b.appspot.com",
+    messagingSenderId: "1016332638211",
+    appId: "1:1016332638211:web:6c6824f0b4b01643119636"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+// Initialize AUTH
+const auth = getAuth();
+
+
+const signupBtn = document.querySelector("#signupBtn")
+signupBtn.addEventListener("click", signUp)
+
+async function signUp(e) {
+    try {
+        const fullName = document.getElementById("fullName").value
+        const phoneNumber = document.getElementById("phoneNumber").value
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+
+
+        const userAuth = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userAuth.user.uid)
+        const uid = userAuth.user.uid
+        const userObj = {
+            fullName,
+            phoneNumber,
+            email,
+            accountActivate: true,
+            uid
+        }
+        console.log("userObj", userObj)
+        const userRef = doc(db, "users", uid);
+        const userDB = await setDoc(userRef, userObj)
+        console.log("userDB", userDB)
+
+        window.location.assign("/")
+
+
+
+
+    } catch (error) {
+        console.log("error", error.message)
+        alert(error.message)
+    }
+
+
+}
