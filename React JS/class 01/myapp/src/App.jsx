50fe00e2128206ae324553ec5e98c214253eb427
Jaffar Aman <amanjaffar50@gmail.com>
2023-08-20 22:44:01 -0700

    react start class 1

---
+++ React JS/class 01/myapp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+    const userName = "JAFFAR AMAN"
+
+
+
+    return (
+        <div  className="" >
+            <h1>HELLO  {userName}  !</h1>
+        </div>
+    )
+
+}
+
+export default App
